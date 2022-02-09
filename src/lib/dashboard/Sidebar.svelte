e3e7e4156272fd40d9fe6d40781f1d620885264a
didiermun <didiermunezer38@gmail.com>
2022-02-09 19:02:06 +0200

    feat: add sidebar

---
+++ src/lib/dashboard/Sidebar.svelte
@@ -0,0 +1,86 @@
+<div class="w-full bg-gray-50 flex flex-col justify-between h-full px-4 py-3 pt-5">
+    <div>
+    <div class="flex flex-col gap-6 mb-10">
+        <div class="bg-white py-5 px-2 flex gap-2 rounded cursor-pointer">
+            <img class="w-10 h-10 rounded-full object-cover" src="https://pbs.twimg.com/profile_images/1490769258271453184/jzkdIjWd_400x400.jpg" alt="">
+            <div class="py-2 font-secondary flex gap-1">
+                <p class="font-semibold text-black">Didier Chummy</p>
+                <img src="/svg/dashboard/drop.svg" class="py-1" alt="">
+            </div>
+        </div>
+        <div class="py-3 px-0 flex gap-2 rounded cursor-pointer">
+            <div class="container flex mx-auto">
+                <div class="flex bg-white rounded">
+                    <button class="flex items-center justify-center px-4">
+                        <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24">
+                            <path
+                                d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z">
+                            </path>
+                        </svg>
+                        
+                    </button>
+                    <input type="text" class="font-secondary px-4 py-4 w-full focus:outline-none text-lg" placeholder="Search...">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col gap-3">
+        <div class="px-3 py-5 bg-white flex justify-between cursor-pointer">
+            <div class="flex gap-3">
+                <img src="/svg/dashboard/notes.svg" alt="">
+                <p class="text-xl font-primary font-semibold text-black">Notes</p>
+            </div>
+            <div class="px-3">
+                <img src="/svg/dashboard/more.svg" alt="">                 
+            </div>
+        </div>
+        <div class="px-3 py-5 bg-white flex justify-between cursor-pointer">
+            <div class="flex gap-3">
+                <img src="/svg/dashboard/todos.svg" alt="">   
+                    
+                <p class="text-xl font-primary font-semibold text-black">Todos</p>
+            </div>
+            <div class="px-3">
+                <img src="/svg/dashboard/more.svg" alt="">                      
+            </div>
+        </div>
+        <div class="px-3 py-5 bg-white flex justify-between cursor-pointer">
+            <div class="flex gap-3">
+                <img src="/svg/dashboard/project.svg" alt="">
+                <p class="text-xl font-primary font-semibold text-black">Projects</p>
+            </div>
+            <div class="px-3">
+                <img src="/svg/dashboard/more.svg" alt="">                 
+            </div>
+        </div>
+        <div class="px-3 py-5 bg-white flex justify-between cursor-pointer">
+            <div class="flex gap-3">
+                <img src="/svg/dashboard/edit.svg" alt="">   
+                    
+                <p class="text-xl font-primary font-semibold text-black">Journal</p>
+            </div>
+            <div class="px-3">
+                <img src="/svg/dashboard/more.svg" alt="">                      
+            </div>
+        </div>
+        <div class="px-3 py-5 bg-white flex justify-between cursor-pointer">
+            <div class="flex gap-3">
+                <img src="/svg/dashboard/list.svg" alt="">
+                <p class="text-xl font-primary font-semibold text-black">Reading List</p>
+            </div>
+            <div class="px-3">
+                <img src="/svg/dashboard/more.svg" alt="">                 
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="">
+        <div class="px-3 py-5 bg-white flex cursor-pointer">
+            <div class="flex gap-3">
+                <img src="/svg/dashboard/settings.svg" alt="">
+                <p class="text-xl font-primary font-semibold text-black">Settings</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/routes/dashboard/__layout.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Sidebar from "$lib/dashboard/Sidebar.svelte";
+</script>
+<div class="grid lg:grid-cols-12 gap-0 bg-white min-h-screen">
+    <div class="col-span-2 h-full">
+        <Sidebar />
+    </div>
+    <div class="col-span-2 bg-white">
+        <div class="container mx-auto px-4 py-8">
+            <h1 class="text-2xl font-bold">Dashboard</h1>
+            <p class="text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum, quisquam.
+            </p>
+        </div>
+    </div>
+    <div class="col-span-4 bg-white">
+        <div class="container mx-auto px-4 py-8">
+            <h1 class="text-2xl font-bold">Dashboard</h1>
+            <p class="text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum, quisquam.
+            </p>
+        </div>
+    </div>
+
+</div>
